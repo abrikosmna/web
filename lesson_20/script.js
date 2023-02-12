2d221085360487cc5eb94052f7bb527ad292fc69
lexasd <atomilov720@gmail.com>
2023-02-12 19:58:25 +0700

    js

---
+++ lesson_20/script.js
@@ -0,0 +1,36 @@
+// function logger(){
+//     let a = 0;
+//     let sum_a = 0;
+//     let sum_b = 0;
+//     for (let i = 0; i < 3; i++) {
+//         a = Number(prompt("Введите для тим деволопер"));
+//         sum_a = sum_a + a;
+//     }
+//     for (let i = 0; i < 3; i++) {
+//         a = Number(prompt("Введите для тим дизайн"));
+//         sum_b = sum_b + a;
+//     }
+//     if (sum_a/3 > sum_b/3) {
+//         console.log("win Develop");
+//         console.log(`Disign не хватило набрать ${sum_a - sum_b} чтобы сравнять счет`);
+//     } 
+//     else if (sum_a === sum_b){
+//         console.log(`${sum_b} = ${sum_a} Победила дружба`);
+//     } 
+//     else {
+//         console.log("win Disign");
+//         console.log(`Develop не хватило набрать ${sum_b - sum_a} чтобы сравнять счет`);
+//     }
+
+// }
+// logger();
+
+
+const name = ["ad", 'ada', 'asda']
+name[20] = "dad"
+console.log(name[6]);
+// name.length;
+// if (a === 0) {
+//        a = "no use";      значение по умолчанию старые версии js
+// }
+// a = a || "no use";        значение по умолчанию старые версии js
