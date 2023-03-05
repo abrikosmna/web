4f5b78261ceecd70d9c4a2b78b68fd155c81b808
Tomyum1 <fuitorsil@gmail.com>
2023-03-05 19:58:06 +0700

    12345678900

---
+++ lesson_23/script.js
@@ -0,0 +1,16 @@
+// let num = Math.trunc( Math.random() * 6) +1;
+// console.log(num);
+
+
+
+// let inp = document.querySelector('.chiki')
+// inp.value = 'Moscow'
+
+// function fun(){
+//     document.querySelector('.chiki').value = "NSK";
+// }
+// Math.ceil // up
+// Math.floor // down
+// Math.round // math
+// Math.trunc // отбрасывание дробной части
+// 5.32412.toFixed(2) // округление после точки
