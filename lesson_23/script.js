9b24b2ea4e3513fbd6231d87396a47bc1250d7b5
alexandroid103 <alexandr.s.ivanoff@gmail.com>
2023-03-12 19:55:20 +0700

    123456

--- lesson_23/script.js
+++ lesson_23/script.js
@@ -1,11 +1,42 @@
 // let num = Math.trunc( Math.random() * 6) +1;
 // console.log(num);
+let num = Math.trunc( Math.random() * 10) +1;
+let inp = document.querySelector('.inp');
+function fun(){
+    let vh = inp.value;
+    console.log(vh, num)
+    let num_f = num;
+    let score =  document.querySelector('.score').value;
+    let record = [];
+    if (vh < num_f)
+    {
+        console.log("О")
+        document.querySelector('.min-max').innerHTML = "загаданное число больше";
+        score =  document.querySelector('.score').value + 1;
+    }
+    else if (vh > num_f) 
+    {
+        console.log("Д")
+        document.querySelector('.min-max').innerHTML = "загаданное число меньше";
+        score =  document.querySelector('.score').value + 1;
+    }
+    else 
+    {
+        console.log("тТ")
+        document.querySelector('.znak').value = num_f;
+        score =  document.querySelector('.score').value + 1;
+        record.push(score);
+        document.querySelector('.record').value = Math.min(record);
+    }
+}
 
 
 
+//score количество попыток угадывания
+//record минимальное количество попыток которые потреповалось для угадывания
 // let inp = document.querySelector('.chiki')
 // inp.value = 'Moscow'
-
+//document.querySelector('.chiki').value = "NSK";
 // function fun(){
 //     document.querySelector('.chiki').value = "NSK";
 // }
